60c5912af9d08fb5b039cac679b4d8035360da45
Harsh Upadhye <harshupadhye2832@gmail.com>
2024-03-12 17:47:45 +0530

    Fixed Analytics Code

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Head from "next/head";
 import { UserProvider } from "@/context/UserContext";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,30 +18,27 @@ export default function RootLayout({ children }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3QG0LVP0DK"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3QG0LVP0DK"
+        />
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-          
+
             gtag('config', 'G-3QG0LVP0DK');
-            `
-          }}
-        />
-          <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-             (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "lecnxhvogt");
-            `
-          }}
-        />
+         `}
+        </Script>
+        <Script strategy="lazyOnload" id="clarity-script">
+          {`
+           (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "lecnxhvogt");
+        `}
+        </Script>
       </Head>
       <UserProvider>
         <body className={inter.className}>
