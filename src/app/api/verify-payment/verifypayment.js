7861b0772100439899fb158876e8ab11d9956c31
Harsh Upadhye <harshupadhye2832@gmail.com>
2024-03-29 19:50:08 +0530

    Build Failed resolved

--- src/app/api/verify-payment/verifypayment.js
+++ src/app/api/verify-payment/verifypayment.js
@@ -1,9 +1,9 @@
 import { stripe } from "@utils/stripe";
 import { NextResponse } from "next/server";
 import getRawBody from "raw-body";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2020-08-27",
-});
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: "2020-08-27",
+// });
 export const config = {
   runtime: "edge",
   unstable_allowDynamic: ["**/node_modules/function-bind/**"],
