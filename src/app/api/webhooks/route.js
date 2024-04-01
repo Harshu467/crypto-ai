f7df988767b7889aa66e770790a34a6a1c5cc618
Harsh Upadhye <harshupadhye2832@gmail.com>
2024-04-01 11:54:38 +0530

    CloudFlare log resloved

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,6 +1,12 @@
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+export const config = {
+  runtime : "edge",
+  api: {
+    bodyParser: false,
+  },
+};
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-08-16",
 });
