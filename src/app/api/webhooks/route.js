a0157b4ff3b327f76a8980bb5f27d10222da1c19
Harsh Upadhye <harshupadhye2832@gmail.com>
2024-04-01 11:56:49 +0530

    syntax fixed

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,8 +1,8 @@
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+export const runtime = "edge";
 export const config = {
-  runtime : "edge",
   api: {
     bodyParser: false,
   },
