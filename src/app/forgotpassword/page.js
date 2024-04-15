2251501da8348f96f7c9321a9a38d411f9cf1a97
Harsh Upadhye <harshupadhye2832@gmail.com>
2024-04-15 23:06:46 +0530

    Forgot Password Added

---
+++ src/app/forgotpassword/page.js
@@ -0,0 +1,164 @@
+"use client";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import EyeOutline from "mdi-material-ui/EyeOutline";
+import EyeOffOutline from "mdi-material-ui/EyeOffOutline";
+import { Toaster, toast } from "react-hot-toast";
+import {
+  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  fetchSignInMethodsForEmail,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  linkWithCredential,
+  setPersistence,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../../../firebase";
+import {
+  addToFirebaseUsers,
+  checkEmailExists,
+} from "../../utils/commonFunctions";
+import { useRouter } from "next/navigation";
+const defaultTheme = createTheme();
+
+export default function ForgotPassword() {
+  const router = useRouter();
+  const [data, setdata] = useState({
+    email: "",
+  });
+
+  const { email } = data;
+
+  const validateForm = () => {
+    const newErrors = {
+      email: "",
+    };
+
+    let formIsValid = true;
+
+    if (!email) {
+      newErrors.email = "Email is required";
+      toast.error(newErrors.email);
+      formIsValid = false;
+      return formIsValid;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "Email is invalid";
+      toast.error(newErrors.email);
+      formIsValid = false;
+      return formIsValid;
+    }
+
+    return formIsValid;
+  };
+
+  const dataChange = (event) => {
+    const newData = { ...data, [event.target.name]: event.target.value };
+    setdata(newData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formIsValid = validateForm();
+    if (!formIsValid) {
+      //console.log("Form is invalid");
+      return;
+    } else {
+      //console.log("Form is valid");
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1637611331620-51149c7ceb94?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyc3x8fHx8fDE3MDczMjM2Mjg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              mt: 8,
+              mx: 2,
+              mb: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Forgot Password
+            </Typography>
+            <Box component="form" noValidate sx={{  }}>
+              <Grid container spacing={2}>
+                <Grid item xs={24}>
+                  <TextField
+                    type="email"
+                    name="email"
+                    label="Email"
+                    fullWidth
+                    value={data.email}
+                    onChange={dataChange}
+                    required
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                onClick={handleSubmit}
+                className="login-button"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Forgot Password
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/login" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      <Toaster position="top-center" reverseOrder={false} />
+    </ThemeProvider>
+  );
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -316,7 +316,7 @@ export default function Login() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
+                  <Link href="/forgotpassword" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid>
